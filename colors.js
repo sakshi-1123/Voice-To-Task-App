0e2dd0a2ec61c6a50623a4cb72c3161fcece13a5
sakshi-1123 <sakshialesarmane@gmail.com>
2026-08-14 22:38:27 +0530

    Theme

---
+++ colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  primary: '#6C5CE7',
+  primaryDark: '#5849c2',
+  secondary: '#00D2A0',
+  background: '#F7F7FC',
+  card: '#FFFFFF',
+  textPrimary: '#1E1E2E',
+  textSecondary: '#6E6E85',
+  border: '#E7E7F2',
+  danger: '#FF6B6B',
+  warning: '#FFB020',
+  success: '#00C48C',
+  recording: '#FF4D67',
+};
+
+export default colors;
